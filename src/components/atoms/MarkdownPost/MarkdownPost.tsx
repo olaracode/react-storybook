a8fd320cea4053a7933ef464ebadaa0a851c6b5c
Octavio Lara <octavioasapchi@gmail.com>
2023-04-13 01:12:06 -0400

    [🎨 Add] Roboto font

--- src/components/atoms/MarkdownPost/MarkdownPost.tsx
+++ src/components/atoms/MarkdownPost/MarkdownPost.tsx
@@ -1,31 +1,50 @@
+import { Box } from "@mui/material";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { materialOceanic } from "react-syntax-highlighter/dist/esm/styles/prism";
-
+import "@fontsource/roboto";
 const MarkdownPost = ({ content }: { content: string }) => {
   return (
-    <ReactMarkdown
-      components={{
-        code({ node, inline, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || "");
-          return !inline && match ? (
-            <SyntaxHighlighter
-              style={materialOceanic}
-              language={match[1]}
-              PreTag="div"
-              children={String(children).replace(/\n$/, "")}
-              {...props}
-            />
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          );
+    <Box
+      sx={{
+        "& > p": {
+          fontFamily: "Roboto",
+          my: 1.5,
+        },
+        "& > h1": {
+          fontFamily: "Roboto",
+        },
+        "& > h2": {
+          fontFamily: "Roboto",
+        },
+        "& > h3": {
+          fontFamily: "Roboto",
         },
       }}
     >
-      {content}
-    </ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={materialOceanic}
+                language={match[1]}
+                PreTag="div"
+                children={String(children).replace(/\n$/, "")}
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {content}
+      </ReactMarkdown>
+    </Box>
   );
 };
 
